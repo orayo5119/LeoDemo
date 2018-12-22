cfa4036014e25b5954289411f055b6712c59c307
Ray Lin <raylin@Rays-MacBook-Pro.local>
2018-12-22 13:30:28 +0800

    clean format

--- src/main.js
+++ src/main.js
@@ -1,45 +1,26 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { Icon, Button } from "react-native-elements";
-
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Icon, Button } from 'react-native-elements';
 
 class Content extends React.Component {
-  render() {
-    const uri =
-      "https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png";
+	render() {
+		const uri = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png';
 
-    return (
-      <View>
+		return (
+			<View>
+				<Text style={{ color: 'red', fontSize: 24, paddingTop: 300 }}>
+					hi Leo, Open up App.js to start working on your app!
+				</Text>
+				<Button title="BUTTON" />
 
+				<Button raised color="yellow" icon={{ name: 'cached' }} title="BUTTON WITH ICON" />
 
+				<Button large rightIcon={{ name: 'code' }} title="LARGE WITH RIGHT ICON" />
 
-        <Text style={{ color: "red", fontSize: 24, paddingTop: 300 }}>
-          hi Leo, Open up App.js to start working on your app!
-        </Text>
-        <Button
-  title='BUTTON' />
-
-<Button
-  raised
-  color="yellow"
-  icon={{name: 'cached'}}
-  title='BUTTON WITH ICON' />
-
-<Button
-  large
-  rightIcon={{name: 'code'}}
-  title='LARGE WITH RIGHT ICON' />
-
-<Button
-  large
-  icon={{name: 'envira', type: 'font-awesome'}}
-  title='LARGE WITH ICON TYPE' />
-
-      </View>
-    );
-  }
+				<Button large icon={{ name: 'envira', type: 'font-awesome' }} title="LARGE WITH ICON TYPE" />
+			</View>
+		);
+	}
 }
-
-
 
 export default Content;
